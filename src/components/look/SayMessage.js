9f075ba77997a6a194b1f577d062782424bdc2a7
dilipsuthar60 <dilipsuthar212000@gmail.com>
2024-03-30 13:17:21 +0530

    look up message section

--- src/components/look/SayMessage.js
+++ src/components/look/SayMessage.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const SayMessage = () => {
+const SayMessage = ({ comp_id }) => {
+  const displayMessage = () => {
+    const el = document.getElementById("message-box");
+    const el2 = document.getElementById("message-box-1");
+    console.log(el, el2);
+    el.style.display = "block";
+    el.style.position = "relative";
+    el.innerHTML = "hello";
+  };
   return (
-    <div>
-      
-    </div>
-  )
-}
+        <div
+          id={comp_id}
+          className=" w-full cursor-pointer p-2 text-center bg-yellow-400 text-black px-2 py-1 my-2 text-sm"
+          onClick={() => displayMessage()}
+        >
+          Say hello message
+        </div>
+  );
+};
 
-export default SayMessage
+export default SayMessage;
--- src/components/look/SayMessageInTime.js
+++ src/components/look/SayMessageInTime.js
@@ -1,11 +1,27 @@
 import React from 'react'
 
-const SayMessageInTime = () => {
+const SayMessageInTime = ({comp_id}) => {
+  const displayMessage = () => {
+    const el = document.getElementById("message-box");
+    const el2 = document.getElementById("message-box-1");
+    console.log(el, el2);
+    el.style.display = "block";
+    el.style.position = "relative";
+    el.innerHTML = "hello";
+    setTimeout(()=>{
+      el.innerHTML= ""
+      el.style.display ="none"
+    },2000);
+  };
   return (
-    <div>
-      
-    </div>
-  )
+        <div
+          id={comp_id}
+          className=" w-full cursor-pointer p-2 text-center bg-yellow-400 text-black px-2 py-1 my-2 text-sm"
+          onClick={() => displayMessage()}
+        >
+          Say hello for 2 second
+        </div>
+  );
 }
 
 export default SayMessageInTime
