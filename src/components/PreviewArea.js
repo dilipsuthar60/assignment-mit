e01f5911f1f472486f581d9c91155c9541deb254
dilipsuthar60 <dilipsuthar212000@gmail.com>
2024-03-30 11:45:06 +0530

    define proper naming in preview area

--- src/components/PreviewArea.js
+++ src/components/PreviewArea.js
@@ -5,16 +5,16 @@ import "../index.css";
 
 export default function PreviewArea() {
   return (
-    <div id="catSprite" className="catSprite" >
+    <div id="catSprite" className="catSprite m-4" >
       <div className="absolute">
-        <div id={`-div`} >
+        <div id="message-and-catsprite-box">
           <div
-            className="border-2 p-2 ml-3 mb-2 w-auto whitespace-nowrap"
-            id="-message-box"
+            className="hidden border-2 p-2 ml-3 mb-2 w-auto whitespace-nowrap"
+            id="message-box"
           >uhu</div>
           <div
             className="hidden rounded-full border-2 w-4 left-1/2 h-4 ml-3 mb-2 whitespace-nowrap"
-            id="-message-box1"
+            id="message-box-1"
           ></div>
           <CatSprite />
         </div>
