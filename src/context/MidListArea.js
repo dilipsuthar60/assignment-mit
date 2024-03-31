fddb16be3d2fc16c209860bdc005f5dd9d044a0d
dilipsuthar60 <dilipsuthar212000@gmail.com>
2024-03-31 13:03:39 +0530

    fix anti angle rotation

--- src/context/MidListArea.js
+++ src/context/MidListArea.js
@@ -1,12 +1,22 @@
-import React,{ createContext,useState } from "react";
+import React, { createContext, useState } from "react";
 
 export const ListContext = createContext(null);
 
 export const ListProvider = (props) => {
   const [TaskList, setTaskList] = useState([]);
-  const [clockAngle,setClockAngle] = useState(0)
+  const [clockAngle, setClockAngle] = useState(0);
+  const [antiClockAngle, setAntiClockAngle] = useState(0);
   return (
-    <ListContext.Provider value={{ TaskList, setTaskList ,clockAngle,setClockAngle}}>
+    <ListContext.Provider
+      value={{
+        TaskList,
+        setTaskList,
+        clockAngle,
+        setClockAngle,
+        antiClockAngle,
+        setAntiClockAngle,
+      }}
+    >
       {props.children}
     </ListContext.Provider>
   );
