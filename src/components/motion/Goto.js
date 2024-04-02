c2267807ccdf0c6883e378a007d45b4edb97f348
dilipsuthar60 <dilipsuthar212000@gmail.com>
2024-04-02 21:50:59 +0530

    handle NAN in input

--- src/components/motion/Goto.js
+++ src/components/motion/Goto.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 
-const GotoXY = ({componentId}) => {
+const GotoXY = ({ componentId }) => {
   const [step, setStep] = useState({
     x: 0,
     y: 0,
   });
-    
+
   const handleGoto = () => {
     const element = document.getElementById("catSprite");
     element.style.position = "relative";
@@ -15,38 +15,40 @@ const GotoXY = ({componentId}) => {
 
   return (
     <div className="text-center rounded bg-blue-500  p-2 my-3">
-        <div className="grid grid-cols-2 my-2">
-          <div className="text-white text-sm font-semibold"> X</div>
-          <input
-            className="mx-2 p-1 py-0 text-center text-black"
-            type="number"
-            value={step.x}
-            onChange={(e) => {
-                setStep({ ...step, x: parseInt(e.target.value)});
-            }}
-          />
-        </div>
-        <div className="grid grid-cols-2 my-2">
-          <div className="text-white text-sm font-semibold">Y</div>
-          <input
-            className="mx-2 p-1 py-0 text-center text-black"
-            type="number"
-            value={step.y}
-            onChange={(e) => {
-                setStep({ ...step, y: parseInt(e.target.value)});
-            }}
-          />
-        </div>
-        <div
-          id={componentId}
-          className="flex bg-red-600 text-white px-2 py-1 mt-3 mb-1 text-sm cursor-pointer"
-          onClick={() => handleGoto()}
-        >
-          <div className="flex mx-auto">
-          Go to X : {step.x }  Y : {step.y}
-          </div>
+      <div className="grid grid-cols-2 my-2">
+        <div className="text-white text-sm font-semibold"> X</div>
+        <input
+          className="mx-2 p-1 py-0 text-center text-black"
+          type="number"
+          value={step.x}
+          onChange={(e) => {
+            if (e.target.value)
+              setStep({ ...step, x: parseInt(e.target.value) });
+          }}
+        />
+      </div>
+      <div className="grid grid-cols-2 my-2">
+        <div className="text-white text-sm font-semibold">Y</div>
+        <input
+          className="mx-2 p-1 py-0 text-center text-black"
+          type="number"
+          value={step.y}
+          onChange={(e) => {
+            if (e.target.value)
+              setStep({ ...step, y: parseInt(e.target.value) });
+          }}
+        />
+      </div>
+      <div
+        id={componentId}
+        className="flex bg-red-600 text-white px-2 py-1 mt-3 mb-1 text-sm cursor-pointer"
+        onClick={() => handleGoto()}
+      >
+        <div className="flex mx-auto">
+          Go to X : {step.x} Y : {step.y}
         </div>
       </div>
+    </div>
   );
 };
 
